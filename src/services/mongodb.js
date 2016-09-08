804dace58cbb7ab63a791ce96b17fabceae1724c
Davide Bianchi <davide.bianchi@mondora.com>
2016-09-08 11:16:24 +0200

    Fixed mongodb timeout

--- src/services/mongodb.js
+++ src/services/mongodb.js
@@ -2,10 +2,6 @@ import {MongoClient} from "mongodb";
 
 import {MONGODB_URL} from "../config";
 
-var options = {
-    options: {
-        replSet: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}}
-    }
-};
-
-export default MongoClient.connect(MONGODB_URL, options);
+export default MongoClient.connect(MONGODB_URL, {
+    replSet: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}}
+});
