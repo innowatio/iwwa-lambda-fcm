3d3027f9d0aa8c58a473c314f9036141b3694e7c
Davide Bianchi <davide.bianchi@mondora.com>
2016-09-20 16:21:05 +0200

    Removed options from mongo

--- src/services/mongodb.js
+++ src/services/mongodb.js
@@ -2,6 +2,4 @@ import {MongoClient} from "mongodb";
 
 import {MONGODB_URL} from "../config";
 
-export default MongoClient.connect(MONGODB_URL, {
-    replSet: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}}
-});
+export default MongoClient.connect(MONGODB_URL);
